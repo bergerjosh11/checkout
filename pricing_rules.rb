f3840f8ecddb16ce373258e100effd3c792a8b65
Joshua Berger <bergerjosh11@gmail.com>
2024-12-12 08:50:10 -0500

    abstracted prices into CSV so they're not hard coded.

--- pricing_rules.rb
+++ pricing_rules.rb
@@ -1,7 +1,12 @@
 # Defines the pricing rules for applying discount rules.
+require 'csv'
+
 class PricingRules
-  def initialize
+  def initialize(price_file_path)
     @rules = {}
+    @product_rules = {}
+    @price_file_path = price_file_path
+    @item_prices = load_prices
   end
 
   # Add a pricing rule for a specific product.
@@ -10,9 +15,15 @@ class PricingRules
     @rules[product_code] << rule
   end
 
+  # Add rules to a product (for more complex associations).
+  def assign_rules_to_product(product_code, rule_names)
+    @product_rules[product_code] ||= []
+    @product_rules[product_code].concat(rule_names)
+  end
+
   # Apply all rules to calculate the price for a specific item and quantity.
   def apply(item, quantity)
-    base_price = item_prices[item]
+    base_price = @item_prices[item]
 
     # If the item has no price, raise an error
     unless base_price
@@ -33,12 +44,12 @@ class PricingRules
 
   private
 
-  # Define the base prices for each product.
-  def item_prices
-    {
-      "GR1" => 3.11,
-      "SR1" => 5.00,
-      "CF1" => 11.23
-    }
+  # Load prices from a CSV file.
+  def load_prices
+    prices = {}
+    CSV.foreach(@price_file_path, headers: true) do |row|
+      prices[row["ProductCode"]] = row["Price"].to_f
+    end
+    prices
   end
 end
