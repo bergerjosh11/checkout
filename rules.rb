ddf507a6a45f43e4a7264b8a940486e63896a0fc
Joshua Berger <bergerjosh11@gmail.com>
2024-12-12 09:01:27 -0500

    abstracted prices into CSV so they're not hard coded.

--- rules.rb
+++ rules.rb
@@ -1,16 +1,16 @@
 # Define custom rules for discounts.
 module Rules
   def self.bogo_rule
-    lambda do |quantity, price|
+    lambda do |quantity, price, base_price|
       # Buy one get one free
       free_items = quantity / 2
-      price -= free_items * 3.11
+      price -= free_items * base_price
       price
     end
   end
 
   def self.three_or_more_bulk_discount_rule(discount_price)
-    lambda do |quantity, price|
+    lambda do |quantity, price, base_price|
       # Bulk discount for three or more items
       price = quantity * discount_price if quantity >= 3
       price
@@ -18,9 +18,9 @@ module Rules
   end
 
   def self.bulk_discount_percentage_rule(percentage)
-    lambda do |quantity, price|
+    lambda do |quantity, price, base_price|
       # Percentage discount for bulk purchases
-      price = quantity * (11.23 * percentage) if quantity >= 3
+      price = quantity * (base_price * percentage) if quantity >= 3
       price
     end
   end
